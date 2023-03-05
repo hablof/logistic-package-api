7431c94a456c268f9a1aaec2a8f07b8f4fbdcbcb
Konstantin Zhikharev <hablof@yandex.ru>
2023-03-05 23:46:51 +0300

    cfg changed

--- internal/app/repo/event.go
+++ internal/app/repo/event.go
@@ -8,6 +8,6 @@ type EventRepo interface {
 	Lock(n uint64) ([]model.PackageEvent, error)
 	Unlock(eventIDs []uint64) error
 
-	Add(event []model.PackageEvent) error
+	Add(event []model.PackageEvent) error // not used
 	Remove(eventIDs []uint64) error
 }
